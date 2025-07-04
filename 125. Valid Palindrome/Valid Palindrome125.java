b58dc7157e5a25996c22b4409936e48f4d9b15e4
Lalit Patil <lp613408@gmail.com>
2025-07-04 07:57:10 +0530

    docs: add README with problem description and examples

---
+++ 125. Valid Palindrome/Valid Palindrome125.java
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+         int left = 0, right = s.size() - 1;
+        while (left < right) {
+            // Move left to next alphanumeric character
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+            // Move right to previous alphanumeric character
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+
+            // Compare characters in lowercase
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
